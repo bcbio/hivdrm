243d6586205c1aa35a8aec9c80e457a86e168f84
naumenko.sa <evolgenomicslab@gmail.com>
2023-04-21 16:23:49 -0400

    update slurm script

--- hivdrm.sh
+++ hivdrm.sh
@@ -3,7 +3,7 @@
 # https://wiki.rc.hms.harvard.edu/display/O2
 
 #SBATCH --partition=priority        # Partition (queue) priority
-#SBATCH --time=12:00:00              # Runtime in D-HH:MM format, 10:00:00 for hours
+#SBATCH --time=24:00:00              # Runtime in D-HH:MM format, 10:00:00 for hours
 #SBATCH --job-name=hivdrm          # Job name
 #SBATCH -c 20			    # cores
 #SBATCH --mem=20G               # Memory needed per CPU or --mem-per-cpu
@@ -13,10 +13,16 @@
 
 date
 
+. ~/work/bcbio_devel/.bcbio_devel_profile
+
+source activate hivdrm_production
+
 ~/code/hivdrm/hivdrm.py \
 --barcodes $1 \
 --reference $2 \
 --threads 20 \
 $3 $4
+
+source deactivate
 
 date
