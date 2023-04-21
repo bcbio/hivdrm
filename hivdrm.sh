2f5241c11a756e971209b1c82ffc0b1a4858b4d9
naumenko.sa <evolgenomicslab@gmail.com>
2023-04-21 16:18:33 -0400

    add batch script

---
+++ hivdrm.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# https://slurm.schedmd.com/sbatch.html
+# https://wiki.rc.hms.harvard.edu/display/O2
+
+#SBATCH --partition=priority        # Partition (queue) priority
+#SBATCH --time=12:00:00              # Runtime in D-HH:MM format, 10:00:00 for hours
+#SBATCH --job-name=hivdrm          # Job name
+#SBATCH -c 20			    # cores
+#SBATCH --mem=20G               # Memory needed per CPU or --mem-per-cpu
+#SBATCH --output=hivdrm_%j.out     # File to which STDOUT will be written, including job ID
+#SBATCH --error=hivdrm_%j.err      # File to which STDERR will be written, including job ID
+#SBATCH --mail-type=NONE             # Type of email notification (BEGIN, END, FAIL, ALL)
+
+date
+
+~/code/hivdrm/hivdrm.py \
+--barcodes $1 \
+--reference $2 \
+--threads 20 \
+$3 $4
+
+date
